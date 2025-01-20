2d88e1959e2b9a2df141319674433e6da47d7cc3
Shefii-02 <shefii.km@gmail.com>
2025-01-20 12:31:58 +0530

    product details

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -28,7 +28,7 @@ class Product extends Model
         return $this->hasMany(VariationKey::class);
     }
 
-
+    
     public function getStatusTextAttribute()
     {
         return $this->status == 1 ? '<span class="text-success"> Published </span>' : '<span class="text-danger"> Drafted </span>';
@@ -43,44 +43,37 @@ class Product extends Model
     }
 
 
-    public function option()
-    {
+    public function option(){
         return $this->hasMany(Option::class);
     }
 
 
-    public function images()
-    {
+    public function images(){
         return $this->hasMany(ProductImage::class);
     }
 
-    public function bestSellProduct()
-    {
+    public function bestSellProduct(){
         return $this->hasOne(BestSellProduct::class);
     }
-    public function featuredProduct()
-    {
+    public function featuredProduct(){
         return $this->hasOne(FeaturedProduct::class);
     }
 
-    public function getMinPriceAttribute()
-    {
+    public function getMinPriceAttribute(){
         return $this->product_variation->min('price');
     }
 
 
     public function getMainThumbImageAttribute()
     {
-        $image =  $this->images->where('type', 'Thumbnail')->first()
-            ?? $this->images->where('type', 'Main Image')->first()
+        return $this->images->where('type', 'Thumbnail')->first() 
+            ?? $this->images->where('type', 'Main Image')->first() 
             ?? $this->images->where('type', 'Extra Image')->first();
-
-
-        return  $image != '' ? $image : '/images/default.png';
     }
 
-    public function variationList()
-    {
-        return $this->hasMany('App\Models\VariationKey', 'product_id', 'id');
+    public function variationList() {
+        return $this->hasMany('App\Models\VariationKey','product_id','id');
     }
+    
+
 }
