da015b5d7b11bae7b7040836740b0dedd60f9d5a
Shefii-02 <shefii.km@gmail.com>
2025-01-20 16:45:51 +0530

    banner

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <!-- Hero Slider Section Start -->
-    <section class="hero-slider"  @detect style="margin-top: 47px;" @else style="margin-top: 35px;" @enddetect >
+    <section class="hero-slider"  @detect style="margin-top: 35px;" @else style="margin-top: 47px;" @enddetect >
         <div class="container">
             <div class="row">
                 <div class="col-12">
