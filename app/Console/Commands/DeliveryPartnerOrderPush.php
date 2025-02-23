a29cda4777b2b1360563a2cacd9f7b53899c66e4
Shefii-02 <shefii.km@gmail.com>
2025-02-24 00:39:40 +0530

    test

--- app/Console/Commands/DeliveryPartnerOrderPush.php
+++ app/Console/Commands/DeliveryPartnerOrderPush.php
@@ -99,7 +99,7 @@ class DeliveryPartnerOrderPush extends Command
     private function orderPushDataFormat($order)
     {
         return [
-            "orderId" => $order->invoice_id . '2K25',
+            "orderId" => $order->invoice_id,
             "orderSubtype" => "FORWARD",
             "readyToPick" => false,
             "orderCreatedAt" => $order->billed_at,
