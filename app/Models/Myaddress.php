8fe808aa518280e551c96b246ca959d58c6a1e8e
Shefii-02 <shefii.km@gmail.com>
2025-02-24 00:45:29 +0530

    myadddress

--- app/Models/Myaddress.php
+++ app/Models/Myaddress.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Myaddress extends Model
 {
     //
-    protected $fillable = ['user_id','name','address','postalcode','city','province','country','base'];
+    protected $fillable = ['user_id','name','email','mobile','address','pincode','locality','state','country','base'];
 }
