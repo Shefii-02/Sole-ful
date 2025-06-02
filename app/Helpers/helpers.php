c2ec97e82a3f2774970c91a53489954f16f69049
Shefii-02 <shefii.km@gmail.com>
2025-06-02 15:20:20 +0530

    invoice update

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -304,13 +304,34 @@ if (!function_exists('getVariants')) {
 function convertNumberToWords($number)
 {
     $words = [
-        0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four',
-        5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine',
-        10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen',
-        14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
-        17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen',
-        20 => 'twenty', 30 => 'thirty', 40 => 'forty', 50 => 'fifty',
-        60 => 'sixty', 70 => 'seventy', 80 => 'eighty', 90 => 'ninety'
+        0 => 'zero',
+        1 => 'one',
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
+        10 => 'ten',
+        11 => 'eleven',
+        12 => 'twelve',
+        13 => 'thirteen',
+        14 => 'fourteen',
+        15 => 'fifteen',
+        16 => 'sixteen',
+        17 => 'seventeen',
+        18 => 'eighteen',
+        19 => 'nineteen',
+        20 => 'twenty',
+        30 => 'thirty',
+        40 => 'forty',
+        50 => 'fifty',
+        60 => 'sixty',
+        70 => 'seventy',
+        80 => 'eighty',
+        90 => 'ninety'
     ];
 
     $levels = [
@@ -329,7 +350,7 @@ function convertNumberToWords($number)
     }
 
     if ($number < 21) {
-        return $words[$number];
+        return $words[(int)$number];
     }
 
     if ($number < 100) {
